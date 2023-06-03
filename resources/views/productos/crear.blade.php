ab5c80797879b5db8d33c095891c13e5a7d65eed
Anthony <anthonyjwvp@gmail.com>
2023-06-03 17:27:49 -0500

    Prueba de Commit

--- resources/views/productos/crear.blade.php
+++ resources/views/productos/crear.blade.php
@@ -4,7 +4,7 @@
             {{ __('Crear Productos') }}
         </h2>
     </x-slot>
-
+    <h1>PRUEBA DE COMMIT</h1>
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
